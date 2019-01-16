f8fff1f1ec6e1ca46f46bcab3cc26e14ac321c6e
Christian Norrman <christian@faxity.se>
2019-01-16 20:19:01 +0100

    Added kmom10 presentation & report to navbar

--- config/navbar/header.php
+++ config/navbar/header.php
@@ -50,6 +50,11 @@ return [
                         "url" => "redovisning/kmom06",
                         "title" => "Redovisning för kmom06.",
                     ],
+                    [
+                        "text" => "Kmom10",
+                        "url" => "redovisning/kmom10",
+                        "title" => "Redovisning för kmom10.",
+                    ],
                 ],
             ],
         ],
@@ -83,6 +88,11 @@ return [
                         "text" => "Designprinciper",
                         "url" => "rapport/designprinciper",
                         "title" => "Rapport om designprinciper",
+                    ],
+                    [
+                        "text" => "Webbplatsdesign",
+                        "url" => "rapport/webbplatsdesign",
+                        "title" => "Rapport om webbplatsdesign",
                     ],
                 ]
             ]
